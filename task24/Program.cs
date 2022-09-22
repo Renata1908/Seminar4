479cb26c7c61f729cba9ab294be1db0732634f25
Renata19 <renatalsa@mail.ru>
2022-09-22 17:31:37 +0700

    24

---
+++ task24/Program.cs
@@ -0,0 +1,35 @@
+﻿// Console.WriteLine("Введите число N (N>0):");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+int ReadNumber(string massage)
+{
+    Console.WriteLine(massage);
+    return Convert.ToInt32(Console.ReadLine()); //тело метода
+}
+
+
+int n = ReadNumber("Введите число:");  //вызов метода
+
+int SumNumbersToA(int a)
+{
+    if(a >= 1)
+    {
+        int sum = 0;
+        for (int i = 1; i <= a; i++)
+        {
+            sum+= i;
+        }
+        return sum; // вернем накопившуюся сумму
+    }
+else 
+{
+    Console.WriteLine("введено некорректное число");
+    return -1; // неправильная сумма
+}
+}
+
+int summa = SumNumbersToA(n);
+Console.WriteLine(summa);
+
+
+
