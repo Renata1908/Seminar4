98fc83897ef4ca4a8b77c3ecc1fd43655a5a9b72
Renata19 <renatalsa@mail.ru>
2022-09-22 18:34:07 +0700

    26

---
+++ task26/Program.cs
@@ -0,0 +1,19 @@
+﻿int ReadNumber(string massage)
+{
+    Console.WriteLine(massage);
+    return Convert.ToInt32(Console.ReadLine()); //тело метода
+}
+
+int NumberOfDigits(int n)
+{
+    int i = 0;
+for (i = 0; n > 0; i++)
+{
+    n = n / 10;
+}
+return i;
+}
+
+int number = ReadNumber("Введите число:");  //вызов метода
+int digits = NumberOfDigits(number); // вызов метода
+Console.WriteLine(digits);
