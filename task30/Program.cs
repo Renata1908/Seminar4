4b9d891855edf52edf67bc5d23ef3ef61feeb077
Renata19 <renatalsa@mail.ru>
2022-09-23 12:14:22 +0700

    30

---
+++ task30/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] array = new int[8];
+Random rand = new Random();
+for (int n = 0; n < array.Length; n++)
+array[n] = rand.Next(0, 2);
+
+foreach (int arr in array)
+{
+ Console.Write(arr + " ");   
+}
