d8ff0b653ca5d9a834d8926956fbf823be09df2a
Renata19 <renatalsa@mail.ru>
2022-09-23 12:02:48 +0700

    28

---
+++ task28/Program.cs
@@ -0,0 +1,32 @@
+﻿int ReadNumber(string massage)
+{
+    Console.WriteLine(massage);
+    return Convert.ToInt32(Console.ReadLine()); //тело метода
+}
+
+
+int SumNumbersToA(int a)
+{
+    if(a >= 1)
+    {
+        int sum = 1;
+        for (int i = 1; i <= a; i++)
+        {
+            sum = sum * i;
+        }
+        return sum; // вернем накопившуюся сумму
+    }
+else 
+{
+    Console.WriteLine("введено некорректное число");
+    return -1; // неправильная сумма
+}
+}
+int n = ReadNumber("Введите число:");  //вызов метода
+int summa = SumNumbersToA(n); // вызов метода
+Console.WriteLine(summa);
+
+
+
+
+
